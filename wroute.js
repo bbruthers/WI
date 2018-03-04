4ddfa3b10c28656ac69221dec50fba6c6048aeeb
brett_bruthers <bbruthers@gmail.com>
2018-03-04 15:13:35 -0500

    ini

--- wroute.js
+++ wroute.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var hbs = require('hbs');
 
 var app = express();
+app.set('view engine', 'hbs');
+
 
 app.listen('2143', function cbListener() {
+    console.log(hbs.VERSION);
     console.log('Server started on port 2143');
 });
